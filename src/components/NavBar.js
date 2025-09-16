52ee0733ccfe218c0be541296888ba11c4716404
mosszion <musdzion@gmail.com>
2025-09-15 22:47:48 -0400

    cleaned my code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from '../images/logo.svg'
-import {  socialLinks } from '../data'
+
 import PageLinks from './PageLinks'
 import SocialLinks from './SocialLinks'
 
